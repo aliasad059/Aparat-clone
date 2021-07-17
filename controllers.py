54270213ad9bf9163d6012c050fae699827b638d
ali asad <aliasad059@gmail.com>
2021-07-17 18:49:11 +0430

    add: new triggers to update film details

--- controllers.py
+++ controllers.py
@@ -1,1 +1,31 @@
 # triggers and transactions
+create_update_film_viewers_trigger = """
+CREATE TRIGGER UpdateViewers
+    AFTER Update
+    ON watch_film
+    FOR EACH ROW
+BEGIN
+    if NEW.has_finished = true and OLD.has_finished = false
+    then
+        update film
+        set film.viewers = film.viewers + 1
+        where film.id = NEW.film_id;
+    end if;
+END;
+"""
+
+create_update_film_rate_avg_trigger = """
+CREATE TRIGGER UpdateAvgRate
+    AFTER INSERT
+    ON film_comment
+    FOR EACH ROW
+BEGIN
+    update film
+    set film.rates = film.rates + 1
+    where film.id = NEW.film_id;
+
+    update film
+    set film.rate_avg = (film.rate_avg * (film.rates - 1) + NEW.rate) / film.rates
+    where film.id = NEW.film_id;
+END;
+"""

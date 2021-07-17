c96ed29c0dfaf4414c57d8827f607aea9e4599e5
ali asad <aliasad059@gmail.com>
2021-07-17 18:50:04 +0430

    add: signin signup function of CLI

--- functions.py
+++ functions.py
@@ -1,1 +1,44 @@
 # functions that are needed to interact with database
+from mysql.connector import Error
+
+
+def signIn(logger, cursor, option, username: str, password: str):
+    if option in ('-a', '--admin'):
+        try:
+
+            cursor.callproc('Admin_SignIn', args=(username, password))
+            logger.info(f'[SignIn] Admin <{username}> successfully signed in.')
+            return True
+
+        except Error as e:
+            logger.error(f'[SignIn] {e.msg}')
+            return False
+    elif option in ('-u', '--user'):
+        try:
+
+            cursor.callproc('User_SignIn', args=(username, password))
+            logger.info(f'[SignIn] User <{username}> successfully signed in.')
+            return True
+
+        except Error as e:
+            logger.error(f'[SignIn] {e.msg}')
+            return False
+
+
+def signUp(logger, cursor, username: str, password: str,
+           first_name: str, last_name: str, email: str,
+           phone_number: str, melli_code: str):
+    try:
+        res = cursor.callproc('SignUp',
+                              args=(username, password, first_name, last_name, email, phone_number, melli_code, ''))
+        res_str = res[7]
+        if res_str == '':
+            logger.info(f'[SignUp] User <{username}> successfully signed up.')
+            return True
+        logger.error(f'[SignUp] ' + res_str)
+        return False
+
+
+    except Error as e:
+        logger.error(f'[SignUp] {e.msg}')
+        return False
